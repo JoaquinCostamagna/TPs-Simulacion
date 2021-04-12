3df98c21da6e404c633f6f6bb59632695190e9db
Agustin Maubecin <69209606+maube1905@users.noreply.github.com>
2021-04-12 17:02:03 -0300

    Inicio de form para chi cuadrado

---
+++ GeneracionDeNumerosAleatorios/GeneracionDeNumerosAleatorios/NumerosAleatorios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Documents;
+
+namespace GeneracionDeNumerosAleatorios
+{
+    class NumerosAleatorios
+    {
+        private IList<double> numeros = new List<double>();
+
+        public NumerosAleatorios(IList<double> num)
+        {
+            numeros = num;
+        }
+    }
+
+    
+   
+}
